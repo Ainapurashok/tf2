51bfe19ae1dce35118a9c4f9145aefaef8741f55
EC2 Default User <ec2-user@ip-172-31-33-49.ap-south-1.compute.internal>
2023-07-19 13:16:07 +0000

    added

---
+++ main_git.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.8.0"
+    }
+  }
+}
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA2CLAYI6FNPMCYGYF"
+  secret_key = "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"
+}
+
+# Setup webserver ec2 instance
+
+resource "aws_instance" "webserver" {
+  ami           = "ami-0d13e3e640877b0b9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "webserver_git"
+  }
